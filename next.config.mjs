a50292afdcf554f89481625b3bad62cc8ec1169a
adars124 <lonleyau@gmail.com>
2024-10-01 14:53:21 +0545

    chore: fix basic configs and install required deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Add a rule to handle .node files
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader', // Ensure you have node-loader installed
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**', // Allow all paths from this hostname
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.iran.liara.run',
+        pathname: '/public/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
